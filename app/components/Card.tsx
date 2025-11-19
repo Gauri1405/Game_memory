57b0b0c067238adfe42886f5311b7f8d7b10b51b
gauri yadwadkar <gauriyadwadkar5577@gmail.com>
2025-11-20 00:21:14 +0530

    Fix Card component and add vercel.json

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-export default function Card({ card, onClick }) {
+type CardItem = {
+  flipped: boolean;
+  matched: boolean;
+  image: string;
+};
+
+type Props = {
+  card: CardItem;
+  onClick: () => void;
+};
+
+export default function Card({ card, onClick }: Props) {
   return (
     <div
       onClick={onClick}
-      className={`w-24 h-24 flex items-center justify-center border rounded-lg cursor-pointer text-white text-xl
-      ${card.flipped || card.matched ? "bg-blue-500" : "bg-gray-700"}
-      `}
+      className={`w-24 h-24 rounded-md cursor-pointer flex items-center justify-center 
+      bg-white transition-all duration-300
+      ${card.flipped || card.matched ? "border-2 border-blue-500" : "border-2 border-gray-700"}`}
     >
       {card.flipped || card.matched ? (
-        <img
-          src={card.image}
-          alt="card"
-          className="w-full h-full object-cover rounded-lg"
-        />
+        <img src={card.image} alt="card" className="w-full h-full object-cover rounded-md" />
       ) : (
-        "?"
+        <div className="w-full h-full bg-gray-900 rounded-md"></div>
       )}
     </div>
   );
